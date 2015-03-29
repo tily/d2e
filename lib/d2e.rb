76b4baec7b36c6c5974e95a92d19d7b75b9fa43b
tily <tily05@gmail.com>
2015-03-29 13:41:17 +0900

    adapt to multiple primary keys

--- lib/d2e.rb
+++ lib/d2e.rb
@@ -2,47 +2,61 @@ require "d2e/version"
 
 class D2E
   def initialize(options)
-    @id = options[:id]
+    @id = [options[:id]].flatten
   end
 
-  def diff(prev, curr)
+  def d2e(prev, curr)
     events = []
 
-    prev_ids = prev.map {|item| item[@id] }
-    curr_ids = curr.map {|item| item[@id] }
+    deleted = difference(prev, curr)
+    created = difference(curr, prev)
+    remained = intersection(prev, curr)
 
-    deleted = prev_ids - curr_ids
-    created = curr_ids - prev_ids
-    updated = prev_ids & curr_ids
-
-    created.each do |id|
-      item = curr.find {|item| item[@id] == id }
+    created.each do |item|
       events << {'type' => 'create', 'item' => item}
     end
 
-    deleted.each do |id|
-      item = prev.find {|item| item[@id] == id }
+    deleted.each do |item|
       events << {'type' => 'delete', 'item' => item}
     end
 
-    updated.each do |id|
+    remained.each do |prev_item, curr_item|
       diff = {}
-      prev_item = prev.find {|item| item[@id] == id }
-      curr_item = curr.find {|item| item[@id] == id }
       keys = prev_item.keys | curr_item.keys
       keys.each do |key|
         prev_value = prev_item[key]
         curr_value = curr_item[key]
         if prev_value != curr_value
-          diff[id] = [prev_value, curr_value]
+          diff[key] = [prev_value, curr_value]
         end
       end
       if !diff.empty?
+        id = Hash[@id.map {|id| [id, prev_item[id]] }]
         events << {'type' => 'update', 'id' => id, 'diff' => diff}
       end
     end
 
     events
+  end
+
+  def difference(list_a, list_b)
+    list = []
+    list_a.each do |item_a|
+      if !list_b.find {|item_b| @id.all? {|id| item_a[id] == item_b[id] } }
+        list << item_a
+      end
+    end
+    list
+  end
+
+  def intersection(list_a, list_b)
+    list = []
+    list_a.each do |item_a|
+      if item_b = list_b.find {|item_b| @id.all? {|id| item_a[id] == item_b[id] } }
+        list << [item_a, item_b]
+      end
+    end
+    list
   end
 end
 
